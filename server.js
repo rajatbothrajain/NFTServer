f8161b590443754d638da1995267576b7378d791
rajatbothrajain <rajatbothrajain@gmail.com>
2022-04-13 23:19:40 +0530

    Update Files

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const Joi = require('joi');
 const express = require('express');
 const app = express();
+const sdk = require('api')('@opensea/v1.0#1j3wv35kyd6wqwc');
 
 //Import middle layer json
 app.use(express.json());
@@ -18,11 +19,30 @@ app.get('/', (req, res)=>{
 
 
 
-app.get('/api/courses/:id', (req, res)=>{
-    const course = courses.find(c => c.id == parseInt(req.params.id));
-    if(!course) res.status(404).send("The course with given id was not found...");
 
-    res.send(course);
+//WalletAddress: 0x1C2DB58d008854e2a77611829c9E7c04De2B411e
+app.get('/api/getNFT/:id', (request, response)=>{
+    
+    var output;
+
+    sdk['retrieving-assets-rinkeby']({
+
+        owner: request.params.id,
+      
+        order_direction: 'desc',
+      
+        offset: '0',
+      
+        limit: '20'
+      
+      })
+        .then(res => response.send(res))
+        .catch(err => response.send(err));
+
+    //const course = courses.find(c => c.id == parseInt(req.params.id));
+    //if(!course) res.status(404).send("The course with given id was not found...");
+    ///console.log("Output:", res);
+    //response.send(res);
 });
 
 app.get('/api/courses/:year/:month', (req, res)=>{
@@ -30,6 +50,11 @@ app.get('/api/courses/:year/:month', (req, res)=>{
 });
 
 app.get('/api/post/', (req, res)=>{
+    res.send(req.query);
+});
+
+app.get('/api/post/', (req, res)=>{
+    
     res.send(req.query);
 });
 
